fc5564c9a40b622719b497d1bb020a0b7cf2b4d9
Jose Arjona <arjonatorres79@gmail.com>
2017-11-18 10:52:17 +0100

    Carga inicial

---
+++ borrar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>confirmación de borrado</title>
+    </head>
+    <body>
+        <?php
+        require 'auxiliar.php';
+
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) ?? false;
+        try {
+            comprobarParametro($id);
+            $pdo = conectar();
+            $fila = buscarPelicula($pdo, $id);
+            ?>
+                <h3>
+                    ¿Seguro que desea borrar la película <?= $fila['titulo'] ?>?
+                </h3>
+                <form action="hacerborrado.php" method="post">
+                    <input type="hidden" name="id" value="<?= $id ?>" />
+                    <input type="submit" value="Sí" />
+                    <input type="submit" value="No"
+                           formaction="index.php" formmethod="get" />
+                </form>
+            <?php
+        } catch (Exception $e) {
+            mostrarErrores($e);
+        }
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Listado de películas</title>
+    </head>
+    <body>
+        <?php
+        require 'auxiliar.php';
+
+        $pdo = conectar();
+        $query = $pdo->query('SELECT * FROM peliculas');
+        // Podemos quitar esta fila porque es iterable
+        // $filas = $query->fetchAll();
+        ?>
+        <div >
+            <table border="1" id="tabla">
+                <thead>
+                    <th>Título</th>
+                    <th>Año</th>
+                    <th>Sinopsis</th>
+                    <th>Duración</th>
+                    <th>Género</th>
+                    <th>Operaciones</th>
+                </thead>
+                <tbody>
+                    <?php foreach ($query as $fila): ?>
+                        <tr>
+                            <td><?= $fila['titulo'] ?></td>
+                            <td><?= $fila['anyo'] ?></td>
+                            <td><?= $fila['sinopsis'] ?></td>
+                            <td><?= $fila['duracion'] ?></td>
+                            <td><?= $fila['genero_id'] ?></td>
+                            <td>
+                                <a href="borrar.php?id=<?= $fila['id'] ?>">
+                                    Borrar
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
