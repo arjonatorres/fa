5c51fdeaf38421d1ccc2e912cb959b58931940e1
Jose Arjona <arjonatorres79@gmail.com>
2017-12-05 15:41:07 +0100

    Borra desde borrar.php

--- borrar.php
+++ borrar.php
@@ -30,16 +30,20 @@
                         comprobarParametro($id, $error);
                         $pdo = conectar();
                         $fila = buscarPelicula($pdo, $id, $error);
-                        comprobarErrores($error);
+                        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                            borrarPelicula($pdo, $id, $error);
+                            comprobarErrores($error);
+                            $_SESSION['mensaje'] = 'Película eliminada correctamente.';
+                            header('Location: index.php');
+                            return;
+                        }
                         ?>
                             <h3>
                                 ¿Seguro que desea borrar la película <b>"<?= $fila['titulo'] ?>"</b>?
                             </h3>
-                            <form action="hacerborrado.php" method="post">
-                                <input type="hidden" name="id" value="<?= $id ?>" />
-                                <input class="btn btn-warning" type="submit" value="Sí" />
-                                <input class="btn btn-default" type="submit" value="No"
-                                       formaction="index.php" formmethod="get" />
+                            <form action="borrar.php?id=<?= $id ?>" method="post">
+                                <input class="btn btn-success" type="submit" value="Sí" />
+                                <a class="btn btn-default" href="index.php">No</a>
                             </form>
                         <?php
                     } catch (Exception $e) {
